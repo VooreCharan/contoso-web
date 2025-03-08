d151b88a20632a73b44ede4db4b92c9a15499717
VooreCharan <152898146+VooreCharan@users.noreply.github.com>
2025-03-08 07:28:38 +0000

    new commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -45,6 +45,12 @@ export const Header = async ({
             className="rounded-full"
           />
         </div>
+        <div>
+      <a href="/signup"
+      className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600">
+      Sign Up
+      </a>
+      </div>
       </div>
     </Block>
   );
